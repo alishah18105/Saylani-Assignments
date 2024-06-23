7a645f32d185e00c31cdff0cac9485264245a9a0
alishah18105 <alishah18105@gmail.com>
2024-06-23 16:41:38 +0500

    Assignment 2

--- great.dart
+++
@@ -1,9 +0,0 @@
-//Create a list of numbers & write a program to get the smallest & greatest
-//number from a list.
-
-void main(){
-  List num = [18,29, 3,14,21,32,11];
-  num.sort();
-  print("The greatest number in the list is ${num.last}");
-  print("The smallest number in the list is ${num.first}");
-}
--- maxValue.dart
+++
@@ -1,9 +0,0 @@
-//Q.9: Given a list of integers, write a dart code that returns the maximum value
-//from the list.
-
-void main(){
-  List <int> num = [32, 15, 40, 21, 75, 32];
-  num.sort();
-  print("The Maximum Value in the list is: ${num.last}");
-
-}
